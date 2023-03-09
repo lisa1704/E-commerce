a7ed87c7a9cf40d1697f78131abb543f43bf0f38
lisa1704 <fahmidatasnim@iut-dhaka.edu>
2023-03-09 20:47:26 +0600

    #routing

---
+++ src/routers/Router.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Routes,Route} from 'react-router-dom';
+import Cart from "../pages/Cart";
+import Home from '../pages/Home';
+import AdminProfile from '../pages/AdminProfile';
+import AdminCustomerList from '../pages/AdminCustomerList'
+import Shop from "../pages/Shop";
+import SignUp from "../pages/SignUp"
+import Login from "../pages/Login"
+const Router = () =>{
+     return (
+     <Routes>
+         <Route exact path='home' element={<Home/>}/>
+         <Route exact path='shop' element={<Shop/>}/>
+         <Route path='home/signup' element={<SignUp/>}/>
+         <Route path='home/signup/login' element={<Login/>}/>
+         <Route path='cart' element={<Cart/>}/>
+         <Route path='admin/admin_customer_list' element={<AdminCustomerList/>}/>
+         <Route exact path='admin' element={<AdminProfile/>}/>
+     </Routes>
+     )
+}
+
+export default Router;
